39b53033a563a9afd124448b88b450a1fd98f7b4
Joonas Reinikka <joonas.reinikka@gmail.com>
2018-11-26 00:25:42 +0200

    Add background video

--- src/containers/TeaserPage.js
+++ src/containers/TeaserPage.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 
+import Video from '../assets/videos/epic1.mp4';
 import Countdown from '../components/Countdown';
 
+const BackgroundVideo = styled.video`
+  position: fixed;
+  right: 0;
+  bottom: 0;
+  min-width: 100%;
+  min-height: 100%;
+`;
+
 class TeaserPage extends Component {
+  videoRef = React.createRef();
+
   render() {
     return (
       <div className="App">
+        <BackgroundVideo autoPlay muted loop ref={this.videoRef}>
+          <source src={Video} type="video/mp4" />
+        </BackgroundVideo>
         <Countdown date={new Date('2019-02-31T00:00:00')} />
       </div>
     );
