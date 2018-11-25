40174a9df557c834d962dc2a358cbd7ee215b0e3
Joonas Reinikka <joonas.reinikka@gmail.com>
2018-11-25 15:59:09 +0200

    Create initial Teaserpage

---
+++ src/containers/TeaserPage.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import Countdown from '../components/Countdown';
+
+class TeaserPage extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Countdown />
+      </div>
+    );
+  }
+}
+
+export default TeaserPage;
