741630106c8c6e4d13eae19645ca71b02a1bcf80
Joonas Reinikka <joonas.reinikka@gmail.com>
2018-11-27 00:09:19 +0200

    Add icons

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import React, { Component } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faVolumeMute, faVolumeUp } from '@fortawesome/free-solid-svg-icons';
 
-import TeaserPage from "./containers/TeaserPage";
+import TeaserPage from './containers/TeaserPage';
+
+library.add(faVolumeUp, faVolumeMute);
 
 class App extends Component {
   render() {
