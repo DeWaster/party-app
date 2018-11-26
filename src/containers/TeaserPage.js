a3d2997de5579c77881ef52efeda67a49c1094e0
Joonas Reinikka <joonas.reinikka@gmail.com>
2018-11-27 00:55:04 +0200

    Add fallback music

--- src/containers/TeaserPage.js
+++ src/containers/TeaserPage.js
@@ -3,10 +3,14 @@ import styled from 'styled-components';
 import { Howl, Howler } from 'howler';
 
 import Video from '../assets/videos/epic1.mp4';
-import epicMusic1 from '../assets/music/epic1.ogg';
-import epicMusic2 from '../assets/music/epic2.ogg';
-import epicMusic3 from '../assets/music/epic3.ogg';
-import epicMusic4 from '../assets/music/epic4.ogg';
+import epicMusic1Ogg from '../assets/music/epic1.ogg';
+import epicMusic2Ogg from '../assets/music/epic2.ogg';
+import epicMusic3Ogg from '../assets/music/epic3.ogg';
+import epicMusic4Ogg from '../assets/music/epic4.ogg';
+import epicMusic1Mp3 from '../assets/music/epic1.mp3';
+import epicMusic2Mp3 from '../assets/music/epic2.mp3';
+import epicMusic3Mp3 from '../assets/music/epic3.mp3';
+import epicMusic4Mp3 from '../assets/music/epic4.mp3';
 
 import Countdown from '../components/Countdown';
 import MuteButton from '../components/MuteButton';
@@ -21,7 +25,7 @@ const BackgroundVideo = styled.video`
 
 class TeaserPage extends Component {
   sound = new Howl({
-    src: [epicMusic1],
+    src: [epicMusic1Ogg, epicMusic1Mp3],
     autoplay: true,
     loop: true,
   });
