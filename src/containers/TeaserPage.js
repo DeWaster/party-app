fe25f39080f52c5f9ff462c0744a43a2d7aa142f
Joonas Reinikka <joonas.reinikka@gmail.com>
2018-11-27 01:39:22 +0200

    Add poster and fallback video

--- src/containers/TeaserPage.js
+++ src/containers/TeaserPage.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
-import { Howl, Howler } from 'howler';
+import { Howl } from 'howler';
 
-import Video from '../assets/videos/epic1.mp4';
+import VideoMp4 from '../assets/videos/epic1.mp4';
+import VideoWebm from '../assets/videos/epic1.webm';
 import epicMusic1Ogg from '../assets/music/epic1.ogg';
 import epicMusic2Ogg from '../assets/music/epic2.ogg';
 import epicMusic3Ogg from '../assets/music/epic3.ogg';
@@ -11,6 +12,7 @@ import epicMusic1Mp3 from '../assets/music/epic1.mp3';
 import epicMusic2Mp3 from '../assets/music/epic2.mp3';
 import epicMusic3Mp3 from '../assets/music/epic3.mp3';
 import epicMusic4Mp3 from '../assets/music/epic4.mp3';
+import poster from '../assets/images/poster.png';
 
 import Countdown from '../components/Countdown';
 import MuteButton from '../components/MuteButton';
@@ -37,8 +39,9 @@ class TeaserPage extends Component {
     const playing = this.sound.playing();
     return (
       <div className="App">
-        <BackgroundVideo autoPlay muted loop>
-          <source src={Video} type="video/mp4" />
+        <BackgroundVideo autoPlay muted loop poster={poster}>
+          <source src={VideoMp4} type="video/mp4" />
+          <source src={VideoWebm} type="video/webm" />
         </BackgroundVideo>
         <Countdown date={new Date('2019-01-31T12:00:00.000Z')} />
         <MuteButton playing={playing} onClick={this.togglePlay} />
