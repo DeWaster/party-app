464c3063e1f2cf10272c6ed535fe6e33f2e8b922
Joonas Reinikka <joonas.reinikka@gmail.com>
2018-12-26 18:14:03 +0200

    Change CSS

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
 import styled from 'styled-components';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
