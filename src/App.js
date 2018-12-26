5a96b49057077bde546a83f73f24ac4acbe2cd48
Joonas Reinikka <joonas.reinikka@gmail.com>
2018-12-27 00:27:03 +0200

    Change location of global styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
 import React, { Component } from 'react';
 import moment from 'moment';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { createGlobalStyle } from 'styled-components';
 
 import TeaserPage from './containers/TeaserPage';
 import FrontPage from './containers/FrontPage';
+
+const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css?family=Fjalla+One|Noto+Sans:400,700');
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html,
+  body {
+    min-height: 100%;
+  }
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5 {
+    font-family: 'Fjalla One', sans-serif;
+  }
+
+  body {
+    background: #fff;
+    margin: 0;
+    padding: 0;
+    font-family: 'Noto Sans', sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+`;
 
 class App extends Component {
   render() {
@@ -11,6 +44,7 @@ class App extends Component {
 
     return (
       <div className="App">
+        <GlobalStyle />
         <Router>
           <Route
             path="/"
--- src/index.js
+++ src/index.js
@@ -1,46 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import styled from 'styled-components';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import store from './store';
 
-const StyledApp = styled(App)`
-  @import url('https://fonts.googleapis.com/css?family=Fjalla+One|Noto+Sans:400,700');
-
-  * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
-
-  html,
-  body {
-    min-height: 100%;
-  }
-
-  h1,
-  h2,
-  h3,
-  h4,
-  h5 {
-    font-family: 'Fjalla One', sans-serif;
-  }
-
-  body {
-    background: #ffffff;
-    margin: 0;
-    padding: 0;
-    font-family: 'Noto Sans', sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-  }
-`;
-
 ReactDOM.render(
   <Provider store={store}>
-    <StyledApp />
+    <App />
   </Provider>,
   document.getElementById('root')
 );
