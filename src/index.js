1cdf6efc85fddb40a01cc06afc52ec59480d8c44
Joonas Reinikka <joonas.reinikka@gmail.com>
2018-12-26 16:33:13 +0200

    Convert css to styled-components

--- src/index.js
+++ src/index.js
@@ -1,10 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import styled from 'styled-components';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const StyledApp = styled(App)`
+  @import url('https://fonts.googleapis.com/css?family=Fjalla+One|Noto+Sans:400,700');
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html,
+  body {
+    min-height: 100%;
+  }
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5 {
+    font-family: 'Fjalla One', sans-serif;
+  }
+
+  body {
+    background: #ffffff;
+    margin: 0;
+    padding: 0;
+    font-family: 'Noto Sans', sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+`;
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
