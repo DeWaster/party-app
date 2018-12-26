b368eff5efb8df6583e6fcc16a198b5c93933717
Joonas Reinikka <joonas.reinikka@gmail.com>
2018-12-26 18:14:35 +0200

    Add initial Redux/Sagas

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import styled from 'styled-components';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import partyApp from './reducers';
 
 const StyledApp = styled(App)`
   @import url('https://fonts.googleapis.com/css?family=Fjalla+One|Noto+Sans:400,700');
@@ -36,6 +38,15 @@ const StyledApp = styled(App)`
     -moz-osx-font-smoothing: grayscale;
   }
 `;
+
+const store = createStore(partyApp);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <StyledApp />
+  </Provider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
