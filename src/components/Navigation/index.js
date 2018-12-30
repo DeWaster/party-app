e9545df1cf4bb179d5c51e17e3cffc449ff51f37
Joonas Reinikka <joonas.reinikka@gmail.com>
2018-12-30 17:32:56 +0200

    Add padding to navigation

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -5,6 +5,8 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import { withStyles } from '@material-ui/core/styles';
+
 import config from '../../config';
 
 const Wrapper = styled.div``;
@@ -13,7 +15,12 @@ const AppHeader = styled.h3`
   flex-grow: 1;
 `;
 
+const styles = theme => ({
+  toolbar: theme.mixins.toolbar,
+});
+
 const Navigation = props => {
+  const { classes } = props;
   return (
     <Wrapper>
       <AppBar>
@@ -28,6 +35,7 @@ const Navigation = props => {
           <AppHeader>{config.title}</AppHeader>
         </Toolbar>
       </AppBar>
+      <div className={classes.toolbar} />
     </Wrapper>
   );
 };
@@ -36,4 +44,4 @@ Navigation.propTypes = {
   onToggleSidepanel: PropTypes.func.isRequired,
 };
 
-export default Navigation;
+export default withStyles(styles)(Navigation);
