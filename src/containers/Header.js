40a1fc2f7d7bfbe2bfc0fe0a2e066048605c73df
Joonas Reinikka <joonas.reinikka@gmail.com>
2018-12-30 17:22:53 +0200

    Rename FrontPage to Header

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -3,12 +3,13 @@ import { connect } from 'react-redux';
 import styled from 'styled-components';
 
 import * as uiActions from '../actions/ui';
+import * as apiActions from '../actions/api';
 import Navigation from '../components/Navigation';
 import SideMenu from '../components/SideMenu';
 
 const Wrapper = styled.div``;
 
-class FrontPage extends Component {
+class Header extends Component {
   render() {
     const props = this.props;
     return (
@@ -34,4 +35,4 @@ const mapDispatchToProps = {
 export default connect(
   mapStateToProps,
   mapDispatchToProps
-)(FrontPage);
+)(Header);
