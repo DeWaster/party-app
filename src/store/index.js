9ab10a37eb9c2e48b0f8dfdfd87ac8ff090f6bc5
Joonas Reinikka <joonas.reinikka@gmail.com>
2019-01-06 00:59:47 +0200

    Fix redux-devtools in store

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,7 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 
+import config from '../config';
 import rootReducer from '../reducers';
 import rootSaga from '../sagas';
 
@@ -8,10 +9,13 @@ const sagaMiddleware = createSagaMiddleware();
 
 const store = createStore(
   rootReducer,
-  compose(
-    applyMiddleware(sagaMiddleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  !!window.__REDUX_DEVTOOLS_EXTENSION__
+    ? compose(
+        applyMiddleware(sagaMiddleware),
+        window.__REDUX_DEVTOOLS_EXTENSION__ &&
+          window.__REDUX_DEVTOOLS_EXTENSION__()
+      )
+    : applyMiddleware(sagaMiddleware)
 );
 
 sagaMiddleware.run(rootSaga);
