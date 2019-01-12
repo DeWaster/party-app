140be334ea51c2cbb7db149913e49fcd7897fea4
Joonas Reinikka <joonas.reinikka@gmail.com>
2019-01-12 22:11:46 +0200

    Change AppContainer background

--- src/containers/AppsContainer.js
+++ src/containers/AppsContainer.js
@@ -8,9 +8,18 @@ import Navigation from '../components/Navigation';
 
 const Wrapper = styled.div`
   min-height: 100vh;
-  background: #41295a;
-  background: -webkit-linear-gradient(to bottom, #2f0743, #41295a);
-  background: linear-gradient(to bottom, #2f0743, #41295a);
+  background-color: #e4e4e1;
+  background-image: radial-gradient(
+      at top center,
+      rgba(255, 255, 255, 0.03) 0%,
+      rgba(0, 0, 0, 0.03) 100%
+    ),
+    linear-gradient(
+      to top,
+      rgba(255, 255, 255, 0.1) 0%,
+      rgba(143, 152, 157, 0.6) 100%
+    );
+  background-blend-mode: normal, multiply;
 `;
 
 class AppsContainer extends Component {
