cd070f84e14c229d400d6e328a2d534f5349a28f
Joonas Reinikka <joonas.reinikka@gmail.com>
2019-01-30 22:09:34 +0200

    Add sounds

--- src/components/SoundBoard/sounds.js
+++ src/components/SoundBoard/sounds.js
@@ -4,6 +4,16 @@ import sound3Mp3 from './sounds/sound3.mp3';
 import sound4Mp3 from './sounds/sound4.mp3';
 import sound5Mp3 from './sounds/sound5.mp3';
 import sound6Mp3 from './sounds/sound6.mp3';
+import sound7Mp3 from './sounds/sound7.mp3';
+import sound8Mp3 from './sounds/sound8.mp3';
+import sound9Mp3 from './sounds/sound9.mp3';
+import sound10Mp3 from './sounds/sound10.mp3';
+import sound11Mp3 from './sounds/sound11.mp3';
+import sound12Mp3 from './sounds/sound12.mp3';
+import sound13Mp3 from './sounds/sound13.mp3';
+import sound14Mp3 from './sounds/sound14.mp3';
+import sound15Mp3 from './sounds/sound15.mp3';
+import sound16Mp3 from './sounds/sound16.mp3';
 
 import sound1Webm from './sounds/sound1.webm';
 import sound2Webm from './sounds/sound2.webm';
@@ -11,6 +21,16 @@ import sound3Webm from './sounds/sound3.webm';
 import sound4Webm from './sounds/sound4.webm';
 import sound5Webm from './sounds/sound5.webm';
 import sound6Webm from './sounds/sound6.webm';
+import sound7Webm from './sounds/sound7.webm';
+import sound8Webm from './sounds/sound8.webm';
+import sound9Webm from './sounds/sound9.webm';
+import sound10Webm from './sounds/sound10.webm';
+import sound11Webm from './sounds/sound11.webm';
+import sound12Webm from './sounds/sound12.webm';
+import sound13Webm from './sounds/sound13.webm';
+import sound14Webm from './sounds/sound14.webm';
+import sound15Webm from './sounds/sound15.webm';
+import sound16Webm from './sounds/sound16.webm';
 
 export default [
   [sound1Webm, sound1Mp3],
@@ -19,4 +39,14 @@ export default [
   [sound4Webm, sound4Mp3],
   [sound5Webm, sound5Mp3],
   [sound6Webm, sound6Mp3],
+  [sound7Webm, sound7Mp3],
+  [sound8Webm, sound8Mp3],
+  [sound9Webm, sound9Mp3],
+  [sound10Webm, sound10Mp3],
+  [sound11Webm, sound11Mp3],
+  [sound12Webm, sound12Mp3],
+  [sound13Webm, sound13Mp3],
+  [sound14Webm, sound14Mp3],
+  [sound15Webm, sound15Mp3],
+  [sound16Webm, sound16Mp3],
 ];
